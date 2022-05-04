2af47bfa470fdfe0882fe9172febe41309eadf6f
WAR10RD <axal3241@gmail.com>
2022-05-04 18:37:53 +0530

    [Added] dontnkow

--- app/src/main/java/com/example/centeralperk/presentation/fragment/splash/SplashFragment.kt
+++ app/src/main/java/com/example/centeralperk/presentation/fragment/splash/SplashFragment.kt
@@ -6,15 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.centeralperk.R
 import com.example.centeralperk.app.App
 import com.example.centeralperk.databinding.FragmentSplashBinding
 import com.example.centeralperk.util.AppConstant
 import com.example.centeralperk.util.PreferenceDataStore
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashFragment : Fragment() {
 
     @Inject
@@ -39,21 +43,31 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launch (Dispatchers.IO){
+        var authenticationToken = ""
+
+        /** Getting the authToken fromt the preference dataStore
+         * Storing the authToken in authenticationToken variable
+         * */
+        lifecycleScope.launch(Dispatchers.IO) {
             preferenceDataStore.read(AppConstant.AUTH_TOKEN).collect { authToken ->
-
                 authToken?.let { token ->
-                    if (token.isEmpty()){
-
-                        /** Navigating to login fragment*/
-                        return@collect
-                    }
-                    /** Setting authToken in application class */
-                    app.setAuthToken(token)
-
-                    /** Navigating to home fragment*/
+                    authenticationToken = token
                 }
             }
+        }
+
+        if (authenticationToken == "") {
+            /** Navigating to login fragment*/
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+
+        } else {
+            /** Setting authToken in application class */
+            app.setAuthToken(authenticationToken)
+
+            /** Navigating to home fragment*/
+            findNavController().navigate(
+                R.id.action_splashFragment_to_homeFragment
+            )
         }
     }
 }
