ed9c6b482667f7bb680402b1cb55537176719372
WAR10RD <axal3241@gmail.com>
2022-05-25 12:34:48 +0530

    [Added] Slide Images

---
+++ app/src/main/java/com/example/centeralperk/presentation/fragment/home/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.centeralperk.presentation.fragment.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.centeralperk.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    /** Data binding component */
+    private lateinit var binding: FragmentHomeBinding
+
+    /** ViewModel dependency */
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        /** Data binding */
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
